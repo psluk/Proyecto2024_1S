6035e28e1a126c6d1b1a2903b796d5d8158a9eb7
Paúl Rodríguez García <70735615+psluk@users.noreply.github.com>
2024-03-12 14:37:52 -0600

    Update format

--- database/UsuariosManager.js
+++ database/UsuariosManager.js
@@ -1,19 +1,18 @@
-const dbmgr = require("./DBManager")
-const db = dbmgr.db
+import { db as _db } from "./DBManager";
+const db = _db;
 
 const getUsuarios = () => {
-    try {
-        const query = `SELECT * FROM Usuario`
-        const readQuery = db.prepare(query)
-        const rowList = readQuery.all()
-        return rowList
-    } catch (err) {
-        console.error(err)
-        throw err
-    }
-}
+  try {
+    const query = `SELECT * FROM Usuario`;
+    const readQuery = db.prepare(query);
+    const rowList = readQuery.all();
+    return rowList;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
 
-module.exports = {
-    getUsuarios,
-
-}
+export default {
+  getUsuarios,
+};
