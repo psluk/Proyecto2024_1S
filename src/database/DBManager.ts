e611ae08ae98f10ba5595f2f4be74b9b71d25aa8
Paúl Rodríguez García <70735615+psluk@users.noreply.github.com>
2024-03-12 18:42:03 -0600

    Fix types

--- src/database/DBManager.ts
+++ src/database/DBManager.ts
@@ -1,4 +1,4 @@
-import Database from "better-sqlite3";
+import Database, { Database as BetterSqlite3Database } from "better-sqlite3";
 import { join } from "path";
 
 const dbPath =
@@ -6,7 +6,7 @@ const dbPath =
     ? "./database/gadt-database.db"
     : join(process.resourcesPath, "./gadt-database.db");
 
-const db = new Database(dbPath);
+const db: BetterSqlite3Database = new Database(dbPath);
 db.pragma("journal_mode = WAL");
 
 export { db };
